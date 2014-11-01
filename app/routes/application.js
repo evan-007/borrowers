99a2dcabca3c18c03ccf3e8291d012ba60dec852
Evan Lloyd <evan.u.lloyd@gmail.com>
2014-11-01 11:22:11 -0400

    Fix template outlet error

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,17 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    save: function(){
-      console.log('bubbled to application, save');
-
-      return true;
-    },
-
-    cancel: function(){
-      console.log('bubbled to application, cancel');
-
-      return true;
-    }
-  }
 });
--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,17 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    save:  function() {
-      console.log('+--- save bubbled up to friends route');
-
-      return false;
-    },
-    cancel: function(){
-      console.log('+--- cancel bubbled up to friends route');
-
-      return false;
-    }
-  }
-
 });
